2fa426141790e3cfef5c6d7f41d452fb54af7977
Garrett Girod <girodg@hotmail.com>
2022-05-02 11:18:36 -0400

    pass by ref

--- Day01/Day01/Program.cs
+++ Day01/Day01/Program.cs
@@ -8,11 +8,29 @@ namespace Day01
         {
             int n1 = 5, n2 = 2;
             int sum = Add(n1, n2);
+
+            //$ - interpolated string
+            Console.Write($"{n1} * 3 = ");
+            Factor(ref n1, 3);
+            Console.WriteLine(n1);
+
+            Console.WriteLine(DateTime.Now);
+
             PrintMessage();
             string msg = GetMessage();
             PrintMessage(msg);
         }
 
+        static void Factor(ref int num1, int theFactor)
+        {
+            num1 *= theFactor;
+        }
+
+        static int Add(int num1, int num2)
+        {
+            num1++;
+            return num1 + num2;
+        }
         static string GetMessage()
         {
             Console.Write("What would you like to say? ");
@@ -30,10 +48,6 @@ namespace Day01
             Console.WriteLine(message);
         }
 
-        static int Add(int num1, int num2)
-        {
-            return num1 + num2;
-        }
 
     }
 }

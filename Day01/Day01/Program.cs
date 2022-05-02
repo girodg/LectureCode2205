2847f5b795371aff88644679f622e5e1b85558f7
Garrett Girod <girodg@hotmail.com>
2022-05-02 10:48:35 -0400

    Day 01 challenges

---
+++ Day01/Day01/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Day01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 5, n2 = 2;
+            int sum = Add(n1, n2);
+            PrintMessage();
+            string msg = GetMessage();
+        }
+
+        static string GetMessage()
+        {
+            Console.Write("What would you like to say? ");
+            string usersInput = Console.ReadLine();
+            return usersInput;
+        }
+
+        static void PrintMessage()
+        {
+            Console.WriteLine("Hello Gotham. I am the hero you need!");
+        }
+
+        static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+    }
+}

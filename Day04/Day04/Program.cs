1885d30c7f9898fdae2b8ee579dfc8876bc7155a
Garrett Girod <girodg@hotmail.com>
2022-05-09 10:31:59 -0400

    loading grades

--- Day04/Day04/Program.cs
+++ Day04/Day04/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace Day04
@@ -11,6 +12,40 @@ namespace Day04
             filePath = @"..\..\..\sample.txt";//relative path
             filePath = "sample.txt";//current directory of the app
 
+            SaveGrades(filePath);
+            List<double> grades = LoadGrades(filePath);
+            foreach (double grade in grades) Console.WriteLine(grade);
+        }
+
+        private static List<double> LoadGrades(string filePath)
+        {
+            List<double> grades = new List<double>();
+            if (File.Exists(filePath))
+            {
+                //using (StreamReader sr = new StreamReader(filePath))//1. Open the file
+                //{
+                //    string line = sr.ReadLine();
+                //    string[] data = line.Split('?');
+                //    foreach (var item in data)
+                //    {
+                //        if(double.TryParse(item, out double value))
+                //            grades.Add(value);
+                //    }
+                //}//Ctrl+K,D to fix the formatting
+                //OR...
+                string fileData = File.ReadAllText(filePath);//open,read,close the file
+                string[] data = fileData.Split('?');
+                foreach (var item in data)
+                {
+                    if (double.TryParse(item, out double value))
+                        grades.Add(value);
+                }
+            }
+            return grades;
+        }
+
+        private static void SaveGrades(string filePath)
+        {
             using (StreamWriter sw = new StreamWriter(filePath))//1. open the file
             {
                 //2. write to the file

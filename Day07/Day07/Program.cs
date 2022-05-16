42135bf42812ed33d9269e5ac070de13da3c2ab6
Garrett Girod <girodg@hotmail.com>
2022-05-16 10:55:05 -0400

    fields and properties

---
+++ Day07/Day07/Program.cs
@@ -0,0 +1,20 @@
+﻿using Day07CL;
+using System;
+
+namespace Day07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount acct = new BankAccount();
+
+            //set my routing #
+            acct.RoutingNumber = 12345;//calls the set automatically for me
+            //'value' will be 12345 in the setter
+
+            int routing = acct.RoutingNumber;//calls the get automatically for me
+            Console.WriteLine(routing);
+        }
+    }
+}
---
+++ Day07/Day07CL/Inventory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day07CL
+{
+    public class Inventory
+    {
+        #region Fields (data)
+        private int _capacity;
+        private List<string> _items;
+        #endregion
+    }
+}

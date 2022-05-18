74722490d9d5458dcd205f76ed0f0f018b9500ff
Garrett Girod <girodg@hotmail.com>
2022-05-18 08:35:21 -0400

    Day 08

---
+++ Day08/Day07/Program.cs
@@ -0,0 +1,40 @@
+﻿using Day07CL;
+using System;
+using System.Collections.Generic;
+
+namespace Day07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount acct = new BankAccount("Bank of Garrett", 12345, 67890, 10000);
+            //acct is an instance of a BankAccount
+
+            //set my routing #
+            acct.RoutingNumber = 12345;//calls the set automatically for me
+            //'value' will be 12345 in the setter
+
+            int routing = acct.RoutingNumber;//calls the get automatically for me
+            Console.WriteLine(routing);
+
+            Inventory hiking = new Inventory(3, new List<string>());
+            Inventory backpack = new Inventory(3, new List<string>());
+            backpack.AddItem("map");
+            backpack.AddItem("pickaxe");
+            backpack.AddItem("diamond sword");
+            try
+            {
+                backpack.AddItem("parachute");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            FantasyWeapon sting = new FantasyWeapon(WeaponRarity.Legendary, 100, 1000, 100000);
+            int damage = sting.DoDamage();
+            Console.WriteLine($"We swing sting and do {damage} damage to the rat.");
+        }
+    }
+}
---
+++ Day08/Day07CL/Inventory.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day07CL
+{
+    public class Inventory
+    {
+        #region Fields (data)
+        private int _capacity;
+        private List<string> _items;
+        #endregion
+
+        #region Properties
+        public int Capacity
+        {
+            get { return _capacity; }
+            set { 
+                if(value > 0)
+                    _capacity = value; 
+            }
+        }
+
+        public int Count
+        {
+            get { return _items.Count;}
+        }
+
+        public List<string> Items
+        {
+            get { return _items; }
+            private set { _items = value; }
+        }
+
+        #endregion
+
+        #region Constructors
+        public Inventory(int capacity, List<string> items)
+        {
+            Capacity = capacity;
+            Items = items.ToList();//clone the list
+        }
+        #endregion
+
+        #region Methods
+        public void AddItem(string itemToAdd)
+        {
+            if (Count == Capacity) //is backpack full?
+                throw new Exception("Backpack is full, fool!");
+
+            _items.Add(itemToAdd);
+        }
+        #endregion
+    }
+}
